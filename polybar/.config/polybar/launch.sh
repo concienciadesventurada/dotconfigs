4d9bc58f4a304325fb655a033e48c9c66f3250e7
concienciadesventurada <porquemarx@gmail.com>
2023-02-18 19:19:37 -0600

    bspwm/polybar recognize active and primary monitors

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+# Checks for active monitors, then called as ${env:MONITOR:}
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use 
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
+
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
